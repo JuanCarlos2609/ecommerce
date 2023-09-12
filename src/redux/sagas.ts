cf80ce0918fd330a76c3e5d690ef6b5b015b28c2
Juan Carlos Orta <carlosorta2607@gmail.com>
2023-09-12 12:01:03 -0600

    feix: delete console.logs

--- src/redux/sagas.ts
+++ src/redux/sagas.ts
@@ -1,20 +1,6 @@
-import { put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga/effects';
 import { handleGetProducts } from './Products';
 
-
-// Saga para manejar la acción de incremento
-function* incrementSaga() {
-  yield put({ type: 'INCREMENT' });
-}
-
-// Saga para manejar la acción de decremento
-function* decrementSaga() {
-  yield put({ type: 'DECREMENT' });
-}
-
-// Define tus sagas raíz
 export default function* rootSaga() {
-  yield takeEvery('INCREMENT_ASYNC', incrementSaga);
-  yield takeEvery('DECREMENT_ASYNC', decrementSaga);
   yield takeEvery('GET_LIST_PRODUCTS_SAGA_STARTED', handleGetProducts);
 }
